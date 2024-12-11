849e791d42dab2654dc27186f2b93c6d9969360a
Hugo Marta <150078628+SirAlabar@users.noreply.github.com>
2024-12-11 23:41:04 +0000

    megaphone done

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "Megaphone" << std::endl;
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            std::string str = argv[i];
+            for (size_t j = 0; j < str.length(); j++)
+                std::cout << (char)std::toupper(str[j]);
+        }
+    }
+    std::cout << std::endl;
+    return 0;
 }
