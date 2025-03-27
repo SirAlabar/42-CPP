7f0c1fd70b897b6117cd2f982b43485dc5b83a42
Hugo Marta <150078628+SirAlabar@users.noreply.github.com>
2025-03-27 12:47:41 +0000

    implement cpp03 ex01

---
+++ CPP03/ex01/ClapTrap.cpp
@@ -0,0 +1,113 @@
+
+#include "ClapTrap.hpp"
+
+
+ClapTrap::ClapTrap() : _name("Default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap default constructor called" << std::endl;
+}
+
+
+ClapTrap::ClapTrap(const std::string &name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap " << _name << " constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+    std::cout << "ClapTrap copy constructor called" << std::endl;
+    *this = other;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "ClapTrap " << _name << " destructor called" << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other)
+{
+    std::cout << "ClapTrap assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        _name = other._name;
+        _hitPoints = other._hitPoints;
+        _energyPoints = other._energyPoints;
+        _attackDamage = other._attackDamage;
+    }
+    return (*this);
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+    if (_energyPoints > 0 && _hitPoints > 0)
+    {
+        std::cout << "ClapTrap " << _name << " attacks " << target
+                  << ", causing " << _attackDamage << " points of damage!" << std::endl;
+        _energyPoints--;
+    }
+    else if (_energyPoints == 0)
+    {
+        std::cout << "ClapTrap " << _name << " is out of energy and cannot attack!" << std::endl;
+    }
+    else
+    {
+        std::cout << "ClapTrap " << _name << " is broken and cannot attack!" << std::endl;  
+    }
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (_hitPoints > 0)
+    {
+        if (amount >= _hitPoints)
+            _hitPoints = 0;
+        else
+            _hitPoints -= amount;
+
+        std::cout << "ClapTrap " << _name << " takes " << amount << " points of damage! " 
+                  << "Remaining hit points: " << _hitPoints << std::endl;
+    }
+    else
+    {
+        std::cout << "ClapTrap " << _name << " is already broken and cannot take more damage!" << std::endl;
+    }
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (_energyPoints > 0 && _hitPoints > 0)
+    {
+        _hitPoints += amount;
+        _energyPoints--;
+        std::cout << "ClapTrap " << _name << " repairs itself for " << amount 
+                  << " hit points! Current hit points: " << _hitPoints << std::endl;
+    }
+    else if (_energyPoints == 0)
+    {
+        std::cout << "ClapTrap " << _name << " is out of energy and cannot repair itself!" << std::endl;  
+    }
+    else
+    {
+        std::cout << "ClapTrap " << _name << " is broken and cannot repair itself!" << std::endl;
+    }
+}
+
+std::string ClapTrap::getName() const
+{
+    return (_name);
+}
+
+unsigned int ClapTrap::getHitPoints() const
+{
+    return (_hitPoints);
+}
+
+unsigned int ClapTrap::getEnergyPoints() const
+{
+    return (_energyPoints);
+}
+
+unsigned int ClapTrap::getAttackDamage() const
+{
+    return (_attackDamage);
+}
---
+++ CPP03/ex01/main.cpp
@@ -0,0 +1,44 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main()
+{
+    std::cout << "\n===== CLAPTRAP & SCAVTRAP BATTLE SIMULATION INITIATED =====\n" << std::endl;
+
+    std::cout << ">> Deploying ClapTrap unit CT-1 to the battlefield..." << std::endl;
+    ClapTrap clap1("CT-1");
+    
+    std::cout << "\n>> First combat engagement:" << std::endl;
+    clap1.attack("Rogue Bandit");
+    clap1.takeDamage(5);
+    clap1.beRepaired(3);
+    
+    std::cout << "\n>> Deploying ScavTrap unit ST-1 to reinforce..." << std::endl;
+    ScavTrap scav1("ST-1");
+    
+    std::cout << "\n>> ScavTrap engaging enemies:" << std::endl;
+    scav1.attack("Elite Psycho");
+    scav1.takeDamage(30);
+    scav1.beRepaired(15);
+    scav1.guardGate();
+    
+    std::cout << "\n>> Deploying clone of ScavTrap ST-1..." << std::endl;
+    ScavTrap scav2(scav1);
+    scav2.attack("Badass Nomad");
+    
+    std::cout << "\n>> Testing proper constructor/destructor chain:" << std::endl;
+    {
+        std::cout << ">> Creating a temporary ScavTrap..." << std::endl;
+        ScavTrap tempScav("TEMP-ST");
+        std::cout << ">> Temporary ScavTrap will be destroyed upon exiting this scope..." << std::endl;
+    }
+    
+    std::cout << "\n>> Original ScavTrap engaging in heavy combat:" << std::endl;
+    scav1.takeDamage(80);
+    scav1.attack("Boss Enemy");
+    scav1.beRepaired(50);
+    
+    std::cout << "\n===== SIMULATION COMPLETE =====\n" << std::endl;
+    
+    return 0;
+}
